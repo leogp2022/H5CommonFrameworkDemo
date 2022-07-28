6665b162152066699f40a3328b6a110383ae0d5e
leo <zihao.liu@dragonplus.com>
2022-07-28 20:47:14 +0800

    update game;

---
+++ H5CommonFrameworkDemo/assets/Script/NewScript.ts
@@ -0,0 +1,48 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        cc.sys.capabilities.webp = true;
+
+        // if (cc.sys.isBrowser && cc.sys.os === cc.sys.OS_IOS) {
+        //     this.checkSupport(function (result) {
+        //         cc.sys.capabilities.webp = result;  // overwrite
+        //         console.log("ok.");
+        //     });
+        // }
+
+        cc.director.loadScene("testwebp");
+    }
+
+    start () {
+
+    }
+
+    // update (dt) {}
+    
+    checkSupport(cb) {
+        var webP = new Image();
+        webP.onload = webP.onerror = function () {
+            let isSupported = (webP.height === 2);
+            cb(isSupported);
+        };
+        webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+    }
+}
