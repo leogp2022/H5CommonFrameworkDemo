6403f849848c9a57938731d323c8a06969bf8e41
leo <zihao.liu@dragonplus.com>
2022-08-18 20:19:11 +0800

    update;

--- H5CommonFrameworkDemo/assets/Script/EnterScene.ts
+++ H5CommonFrameworkDemo/assets/Script/EnterScene.ts
@@ -1,9 +1,9 @@
 import { CCGameApp } from "../CFramework/CCCBase/Script/CCGameApp";
+import { CCConfig } from "../CFramework/CCCBase/Script/Config/CCConfig";
 import { ViewManager } from "../CFramework/CCCBase/Script/UI/Base/CViewManager";
-import UIWaiting from "../CFramework/CCCBase/Script/UI/CUIWaiting";
-import { RecommandGameModel } from "../CFramework/CCCBase/Script/UI/RecommandGame/Model/RecommandGameModel";
-import AdLogicModel from "../CFramework/CCCBase/Script/UserGroup/Model/CAdLogicModel";
 import BridgeManager from "../CFramework/CPlugin/Bridge/CBridgeManager";
+import { EventCenter } from "../CFramework/CPlugin/Event/CEventCenter";
+import { EventEnum } from "../CFramework/CPlugin/Event/CEventEnum";
 import { DebugUtils } from "../CFramework/CPlugin/Utils/CDebugUtils";
 import DebugView from "./Game/UI/DebugView";
 import { StoreModel } from "./Game/UI/Store/Model/StoreModel";
@@ -18,6 +18,7 @@ export default class EnterScene extends cc.Component {
         console.log("webp: ", cc.sys.capabilities.webp);
         cc.sys.capabilities.webp = true;
 
+        CCConfig.ENABLE_RECOMMAND_GAME = true;
         CCGameApp.Instance().Init({
             gameScheme: "tileshop",
             configPrePath: "Configs/",
@@ -28,13 +29,11 @@ export default class EnterScene extends cc.Component {
         });
 
         StoreModel.Instance().Init();
-        RecommandGameModel.Instance().Init();
 
         await ViewManager.Instance().openScene(DebugView);
 
         BridgeManager.Instance().scheme_notifyprogress("LandLobby", 1.0);
 
-        
     }
 
     start() {
